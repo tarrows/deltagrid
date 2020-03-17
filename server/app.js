eb1ea6422ec6838ea2004b170ae13f79b1a1a2cd
tarrows <c10t@fa.ke>
2020-03-17 23:39:06 +0900

    introduce socket io

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production'
@@ -11,6 +12,19 @@ const handle = nx.getRequestHandler()
 nx.prepare().then(() => {
   const app = express()
   const server = createServer(app)
+  const io = socketIO(server);
+
+  io.on('connection', socket => {
+    console.log('a user connected.');
+
+    socket.on('SEND_MESSAGE', data => {
+      io.emit('RECEIVE_MESSAGE', data);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('a user disconnected.');
+    });
+  });
 
   app.get('/item', (req, res) => {
     res.send({ hello: 'world' })
